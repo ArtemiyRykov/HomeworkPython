d13c59cbeb3e4723574bf16558d6ace2879d46b4
Artemiy <azazellorart@gmail.com>
2022-07-12 22:11:09 +0500

    Task_4

--- HomeWorkPython_1/task_4.py
+++ HomeWorkPython_1/task_4.py
@@ -4,4 +4,11 @@
 quarter = int(input('Введите номер четверти (от 1 до 4) - '))
 if quarter < 1 or quarter > 4:
     print('Не верное число повторите еще раз (циферки нужно от 1 до 4)')
-    
+elif quarter == 1:
+    print('x > 0 и y > 0')
+elif quarter == 2:
+    print('x < 0 и y > 0')
+elif quarter == 3:
+    print('x < 0 и y < 0')
+elif quarter == 4:
+    print('x > 0 и y < 0')           
